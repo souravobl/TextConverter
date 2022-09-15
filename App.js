b100e9bebbeadb5cacd5894cbbfa0f8a2f2c1a1a
souravobl <112747493+souravobl@users.noreply.github.com>
2022-09-15 12:35:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Convert from "./component/Convert";
+
+function App() {
+  return (
+    <>
+      <div className='container my-4'>
+        <Convert title='Enter sentance:' />
+      </div>
+    </>
+  );
+}
+
+export default App;
